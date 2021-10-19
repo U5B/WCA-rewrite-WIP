9f0990a1dea945b0451bb3ada02896248bc90527
usb <56985400+U5B@users.noreply.github.com>
2021-10-19 13:34:20 -0700

    added motd (green)

--- src/util/regex.js
+++ src/util/regex.js
@@ -8,4 +8,5 @@ const classRegex = '(?<class>Archer|Hunter|Warrior|Knight|Mage|Dark Wizard|Assas
 const e.world = {}
 // regex for joining world
 const e.world.login.string = new Regexp(`^{usernameRegex} has logged into server ${worldRegex} as an? ${classRegex}$`)
+const e.world.login.motd.green = new Regexp (`§a(?:|§a§o)${usernameRegex}§2 has logged into server §a${worldRegex}§2 as §aan? ${classRegex}`)
 module.exports = e
