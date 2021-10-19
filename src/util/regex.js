8ca37ac910f1a24a2675b91ae58915d97054c9f2
usb <56985400+U5B@users.noreply.github.com>
2021-10-19 13:30:12 -0700

    Update regex.js

--- src/util/regex.js
+++ src/util/regex.js
@@ -1,6 +1,11 @@
 const e = {}
 
+// These regexes ignore resets (§r) if parsing with motd
+
+const usernameRegex = '(?<username>[0-9A-Za-z_ ]{1,19})'
+const worldRegex = '(?<world>(?:WC)[0-9]{1,3})'
+const classRegex = '(?<class>Archer|Hunter|Warrior|Knight|Mage|Dark Wizard|Assassin|Ninja)'
 const e.world = {}
 // regex for joining world
-e.world.join = /^(?<username>[0-9A-Za-z_\ ]{1,19}) has logged into server (?<world>(?:WC)[0-9]{1,3}) as an? (?<class>Archer|Hunter|Warrior|Knight|Mage|Dark Wizard|Assassin|Ninja)À?$/
+const e.world.login.string = new Regexp(`^{usernameRegex} has logged into server ${worldRegex} as an? ${classRegex}$`)
 module.exports = e
