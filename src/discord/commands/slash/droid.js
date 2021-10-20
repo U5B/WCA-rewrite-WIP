dbefa1e1104d40e1d3c40aab42c3f71e609aaa54
usb <56985400+U5B@users.noreply.github.com>
2021-10-20 11:11:43 -0700

    hope psuedo code works

--- src/discord/commands/slash/droid.js
+++ src/discord/commands/slash/droid.js
@@ -8,7 +8,27 @@ module.exports = {
     {
       name: 'start',
       description: 'start droid',
-      type: 'SUB_COMMAND'
+      type: 'SUB_COMMAND',
+      options: [
+        {
+          name: 'host',
+          description: 'ip or hostname',
+          type: 'STRING',
+          required: false
+        },
+        {
+          name: 'port',
+          description: 'port',
+          type: 'INTEGER',
+          required: false
+        },
+        {
+          name: 'version',
+          description: 'version',
+          type: 'STRING',
+          required: false
+        }
+      ]
     },
     {
       name: 'stop',
@@ -42,25 +62,34 @@ module.exports = {
     const subCommand = interaction.options.getSubcommand()
     switch (subCommand) {
       case 'start': {
-        interaction.followUp('started a bot')
-        initDroid(true)
+        const options = {
+          host: interaction.options.get('host').value,
+          port: interaction.options.get('port').value,
+          version: interaction.options.get('version').value
+        }
+        interaction.editReply({ content: `started bot with host: ${options.host}; port: ${options.port}, version: ${options.version}`, ephemeral: true })
+        if (options.host === undefined && options.port === undefined && options.version === undefined) {
+          await initDroid(true)
+        } else {
+          await initDroid(true, options)
+        }
         break
       }
       case 'stop': {
-        interaction.followUp('stopped a bot')
+        interaction.editReply('stopped a bot')
         const droid = await returnDroid()
         droid.end()
         break
       }
       case 'say': {
         const string = interaction.options.get('text').value
-        interaction.followUp(`said ${string}`)
+        interaction.editReply(`said ${string}`)
         const droid = await returnDroid()
         droid.chat(string)
         break
       }
       default: {
-        interaction.followUp('how')
+        interaction.editReply('how')
         break
       }
     }
